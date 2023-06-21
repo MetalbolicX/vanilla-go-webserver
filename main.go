9a431b06ca8815c9a2121d77ce81417b940bd548
José Martínez Santana <jose.martinez.santana@gmail.com>
2023-06-20 20:21:47 -0600

    Change for better readability of the if statement

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ import (
 
 func main() {
 
-	err := godotenv.Load(".env")
-	if err != nil {
+	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 	PORT := os.Getenv("SERVER_PORT")
@@ -22,7 +21,6 @@ func main() {
 	STATIC_FOLDER := os.Getenv("STATIC_FOLDER")
 
 	server := server.NewServer(PORT, STATIC_FOLDER)
-	//
 	bindRoutes(server)
 	if err := server.Listen(DB_MANAGEMENT_SYSTEM, DB_URL); err != nil {
 		log.Fatal("Server cannot be started")
